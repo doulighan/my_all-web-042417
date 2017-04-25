751629ee1f35d0b440379fa68f1f34f30b2e9fbf
doulighan <doulighan@gmail.com>
2017-04-24 21:01:58 -0400

    added non-enumerable solution

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all1(collection)
+  i = 0
+  block_return_value = []
+  while i < collection.length
+    block_return_value << yield(collection[i])
+    i += 1
+  end
+  block_return_value.include?(false) ? false : true
 end
+
+def my_all?(collection)
+  i = 0
+  block_return_value = []
+  while i < collection.length
+    return false if !yield(collection[i])
+    i += 1
+  end
+  true
+end
+
